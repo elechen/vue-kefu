7c4e08cdcff5f4a094b560313d2cb249575d1499
elechen <elechen@outlook.com>
2019-11-09 14:47:46 +0800

    #

--- src/define.ts
+++ src/define.ts
@@ -1,2 +1,3 @@
 export const PRODUCTION = process.env.NODE_ENV === 'production';
-export const HOST = PRODUCTION ? '192.168.1.6:8044' : 'localhost:3336';
+// export const HOST = PRODUCTION ? '192.168.1.6:8044' : 'localhost:3336';
+export const HOST = PRODUCTION ? '39.98.51.32:8044' : 'localhost:3336';
