2a06966296534de594dc8d6b11fd58fcb3f20dd9
elechen <elechen@outlook.com>
2019-11-11 20:02:50 +0800

    修改服务器IP

--- src/define.ts
+++ src/define.ts
@@ -1,4 +1,4 @@
 export const PRODUCTION = process.env.NODE_ENV === 'production';
 // export const HOST = PRODUCTION ? '192.168.1.6:8044' : 'localhost:3336';
 // export const HOST = PRODUCTION ? '39.98.51.32:8044' : 'localhost:3336';
-export const HOST = PRODUCTION ? '46.62.106.213:8044' : 'localhost:3336';
+export const HOST = PRODUCTION ? '42.62.106.213:8044' : 'localhost:3336';
