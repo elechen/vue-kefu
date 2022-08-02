761e3660ea47215695218045792e4827e37d1c98
elechen <elechen@outlook.com>
2022-08-02 09:20:47 +0800

    #

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set timeout 3;                       # 设定超时时间为3秒
 spawn scp -r dist root@8.134.39.170:/srv/www/kefu.169youxi.com/
 
 expect "*password*"; 
-send "Sifeng2020\r";                # 向命令行输入密码并回车;
+send "SifengELEL\r";                # 向命令行输入密码并回车;
 
 interact;
